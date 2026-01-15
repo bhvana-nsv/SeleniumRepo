e5bf84d675563490e296bd2930bfaddad5f86926
bhavna-nsv <bhavna.verma@supremetechnologiesindia.com>
2026-01-15 22:59:40 +0530

    Added  static and dynamic Web tables

--- src/test/java/pandaautomate/WebTable.java
+++ src/test/java/pandaautomate/WebTable.java
@@ -17,30 +17,40 @@ public class WebTable {
 		 driver.navigate().to("https://vinothqaacademy.com/webtable/");
 	     driver.manage().window().maximize();
 	     
-	     int rows= driver.findElements(By.xpath("//table[@id=\"myTable\"]//tr")).size();
+	     int rows= driver.findElements(By.xpath("//table[@id='myTable']//tr")).size();
 	     System.out.println("Number of rows:" +rows);
 	     
-	     int col= driver.findElements(By.xpath("//table[@id=\"myTable\"]//th")).size();
+	     int col= driver.findElements(By.xpath("//table[@id='myTable']//th")).size();
 	     System.out.println("Number of Col:" +col);
+	    
+	     driver.findElement(By.xpath("//input[@id='nameInput']")).sendKeys("Hhsgdub Jasue");
+	     driver.findElement(By.xpath("//input[@id='roleInput']")).sendKeys("Project Manager"); 
+	     driver.findElement(By.xpath("//input[@id='emailInput']")).sendKeys("shhgdj@gm.com");
+	     driver.findElement(By.xpath("//input[@id='locationInput']")).sendKeys("New york");
+	     driver.findElement(By.xpath("//input[@id='departmentInput']")).sendKeys("QA Testing");
+	     driver.findElement(By.xpath("//button[@id='addBtn']")).click(); 
+	     
+	     
 	     List <WebElement> checkboxes =driver.findElements(By.xpath("//input[@type='checkbox']"));
 	     
-	     for(int r=2;r<=rows;r++)
+	     for(int i=1;i<6;i++) 
+	        {  
+	        	checkboxes.get(i).click();
+	        } 
+	     
+	     for(int r=1;r<=rows;r++)
 	     {
-	    	for(int c=1;c<=col;c++)
+	    	for(int c=2;c<=col;c++)
 	    	{
-	    		driver.findElement(By.xpath("//table[@id=\"myTable\"]//tr["+r+"]//th[c]")).getText();
-	    		//System.out.println("")
+	    		String value= driver.findElement(By.xpath("//table[@id='myTable']//tr["+r+"]//td["+c+"]")).getText();
+	    		System.out.println(value);
 	    	}
 	     }
-		     
-		     for(int i=1;i<10;i++) 
-		        {  
-		        	checkboxes.get(i).click();
-		        } 
-		      
-	     Thread.sleep(3000);
-	     driver.quit();
-	}
+	     
+	      
+	      Thread.sleep(3000); 
+	     driver.quit(); 
+	 }
 
 }
 
